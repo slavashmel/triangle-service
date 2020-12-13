1be627ea43159a2301a2e45dd519478018288c90
artMelnikov <viacheslav.melnikov@netcracker.com>
2020-12-14 02:28:49 +0400

    testng>junit

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,9 +1,14 @@
 package runner;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-//@RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/java/features"}, plugin = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"}, glue = "steps")
-public class TestRunner extends AbstractTestNGCucumberTests {
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/features",
+        plugin = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"},
+        glue = "steps"
+)
+public class TestRunner {
 }
